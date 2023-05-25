016a38172aedab957b414053bffd2666b8d065c7
zulujive <51298470+zulujive@users.noreply.github.com>
2023-05-25 16:21:22 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,9 +28,9 @@ cp /etc/apt/sources.list /etc/apt/sources.list.bak
 echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
 echo "deb http://deb.debian.org/debian buster-updates main" >> /etc/apt/sources.list
 echo "deb http://security.debian.org/debian-security buster/updates main" >> /etc/apt/sources.list
-apt update
+apt update >/dev/null 2>&1
 echo "Updating Packages..."
-apt upgrade -y
+apt upgrade -y >/dev/null 2>&1
 echo "Packages Upgraded!"
 
 # Apply Settings
