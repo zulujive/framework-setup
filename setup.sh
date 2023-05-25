3158579eaa596137bba96a291b1e150f87458993
zulujive <51298470+zulujive@users.noreply.github.com>
2023-05-25 16:20:24 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,39 +16,33 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Install Necessary Packages
-# Install Necessary Packages
+echo "Installing Packages..."
 apt update >/dev/null 2>&1
 apt install fprintd libpam-fprintd powertop intel-media-va-driver intel-gpu-tools git curl default-jdk zsh -y >/dev/null 2>&1
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >/dev/null 2>&1
+echo "Packages Installed!"
 
 # Ask about installing Debian Buster repos as a failover in case a package is not found in Sid repos
-bash -c '
-read -rp "Do you want to add stable Debian repositories as a failover (sometimes required for installing certain software)? (Y/n) " choice
-choice=${choice:-n}
-choice=${choice,,}
-while [[ ! "${choice}" =~ ^(y|n)$ ]]; do
-    read -p "Please enter Y for Yes or N for No: " choice
-    choice=${choice,,}
-done
-
-if [[ "${choice}" == "y" ]]; then
-    echo "Using stable repos as failover"
-    cp /etc/apt/sources.list /etc/apt/sources.list.bak
-    echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
-    echo "deb http://deb.debian.org/debian buster-updates main" >> /etc/apt/sources.list
-    echo "deb http://security.debian.org/debian-security buster/updates main" >> /etc/apt/sources.list
-    apt update
-else
-    echo "Using unstable repos only"
-fi
-'
+echo "Using stable repos as failover"
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian buster-updates main" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security buster/updates main" >> /etc/apt/sources.list
+apt update
+echo "Updating Packages..."
+apt upgrade -y
+echo "Packages Upgraded!"
 
 # Apply Settings
+echo "Installing GRUB Blacklist"
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="quiet splash module_blacklist=hid_sensor_hub"' >> /etc/default/grub >/dev/null 2>&1
 update-grub >/dev/null 2>&1
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']" >/dev/null 2>&1
+echo "Configuring PowerTOP..."
 powertop --auto-tune >/dev/null 2>&1
 systemctl start powertop >/dev/null 2>&1
 systemctl enable powertop >/dev/null 2>&1
+echo "PowerTOP Configured!"
 echo PCIE_ASPM_ON_BAT=powersupersave >> /etc/tlp.conf >/dev/null 2>&1
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >/dev/null 2>&1
+echo ""
 echo "Successfully installed packages and updated settings. Please restart your system for some settings to take effect."
